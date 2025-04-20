edfeee24a412946ca89035d8fe9e6c49ad810483
Huy Nguyen <huynguyen1393@gmail.com>
2025-04-20 13:59:06 +0700

    Add FSM System

---
+++ Assets/_Project/Scripts/Core/FSM/FSMState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMState
+{
+
+	public virtual void OnEnter ()
+	{
+		
+	}
+
+	public virtual void OnEnter (object data)
+	{
+
+	}
+
+	public virtual void OnFixedUpdate ()
+	{
+
+	}
+
+	public virtual void OnUpdate ()
+	{
+
+	}
+
+	public virtual void OnLateUpdate ()
+	{
+
+	}
+
+	public virtual void OnExit ()
+	{
+
+	}
+
+}
---
+++ Assets/_Project/Scripts/Core/FSM/FSMSystem.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMSystem : MonoBehaviour
+{
+
+	private List<FSMState> lsStates = new List<FSMState> ();
+	public FSMState currentState;
+
+	#region States
+
+	public void AddState (FSMState state)
+	{
+		lsStates.Add (state);
+		if (lsStates.Count == 1) {
+			currentState = state;
+			currentState.OnEnter ();
+		}
+	}
+
+	public void GotoState (FSMState state)
+	{
+		if (currentState != null) {
+			currentState.OnExit ();
+		}
+		currentState = state;
+		currentState.OnEnter ();
+	}
+
+	public void GotoState (FSMState state, object data)
+	{
+		if (currentState != null) {
+			currentState.OnExit ();
+		}
+		currentState = state;
+		currentState.OnEnter (data);
+	}
+
+	#endregion
+
+	#region Unity function
+
+	// Use this for initialization
+	void Start ()
+	{
+		OnSystemStart ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		OnSystemUpdate ();
+		if (currentState != null) {
+			currentState.OnUpdate ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		OnSystemFixedUpdate();
+		if (currentState != null) {
+			currentState.OnFixedUpdate ();
+		}
+	}
+
+	void LateUpdate ()
+	{
+        OnSystemLateUpdate();
+		if (currentState != null) {
+			currentState.OnLateUpdate ();
+		}
+	}
+
+	public virtual void OnSystemUpdate ()
+	{
+		
+	}
+    public virtual void OnSystemLateUpdate()
+    {
+
+    }
+	public virtual void OnSystemStart ()
+	{
+
+	}
+
+	public virtual void OnSystemFixedUpdate()
+    {
+
+    }
+	#endregion
+}
