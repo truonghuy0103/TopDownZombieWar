5e05c9ea029bdd70b64bf1017d33f92b7b7e2f8a
Huy Nguyen <huynguyen1393@gmail.com>
2025-04-21 08:46:36 +0700

    Handle fire gun by mouse

--- Assets/_Project/Scripts/Player/PlayerController.cs
+++ Assets/_Project/Scripts/Player/PlayerController.cs
@@ -9,9 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private WeaponControl _weaponControl;
     [SerializeField] private float _speedMove = 2;
     [SerializeField] private Transform _transModel;
-
-    private bool _isFire;
-
+    
     private void Awake()
     {
         ConfigManager.Instance.InitConfig(() =>
@@ -29,15 +27,20 @@ public class PlayerController : MonoBehaviour
     
     private void Update()
     {
-        Vector3 moveDir = new Vector3(_playerInput.dir.x,0,_playerInput.dir.y);
-        moveDir.Normalize();
+        Vector3 moveDir = new Vector3(_playerInput.dir.x, 0, _playerInput.dir.y);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 999))
+        {
+            Vector3 target = hitInfo.point;
+            target.y = _transModel.position.y;
+            _transModel.LookAt(target);
+        }
         
         if (moveDir.magnitude > 0)
         {
-            transform.forward = moveDir;
-            _transModel.forward = moveDir;
-            
-            _characterController.Move(transform.forward * (Time.deltaTime * _speedMove));
+            moveDir.Normalize();
+            //_transModel.forward = moveDir;
+            _characterController.Move(moveDir * (Time.deltaTime * _speedMove));
         }
         _playerDataBinding.Speed = moveDir.magnitude;
     }
--- Assets/_Project/Scripts/Player/PlayerInput.cs
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -26,13 +26,13 @@ public class PlayerInput : MonoBehaviour
     private void Update()
     { 
         dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            IsFire = true;
+            IsFire = true;          
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
         {
             IsFire = false;
         }
